f4ce9fc09aeac8425b6d960c371480f28d06b6a0
elsehow <yes@cosmopol.is>
2017-08-14 12:38:56 -0700

    Types + tests

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,22 +1,42 @@
 package tutorial.webapp
 
-import scala.scalajs.js.annotation.JSExportTopLevel
+// import scala.scalajs.js.annotation.JSExportTopLevel
 
-import org.scalajs.jquery.jQuery
+class User(username: String) {
+  // require(d != 0)
+  val name: String = username
+  val online: Boolean = false
+  override def toString = name
+}
 
-object TutorialApp {
-  def main(args: Array[String]): Unit = {
-    jQuery(() => setupUI())
-  }
+class Comment(u: User, s: String) {
+  val poster: User = u
+  val body: String = s
+  override def toString = s"${poster} - ${body}"
+  // TODO timestamp required...?
+}
 
-  def setupUI(): Unit = {
-    jQuery("""<button type="button">Click me!</button>""")
-      .click(() => addClickedMessage())
-      .appendTo(jQuery("body"))
-    jQuery("body").append("<p>Hello World</p>")
-  }
 
-  def addClickedMessage(): Unit = {
-    jQuery("body").append("<p>You clicked the button!</p>")
-  }
+class Post(u: User, l: String, t: Option[String]) {
+  val poster: User = u
+  val url: String = l
+  val title: Option[String] = t
+  val comments: List[Comment] = Nil
+  // TODO timestamp required...?
+}
+
+class Room(c: User, n: String) {
+  val creator: User = c
+  val name: String = n
+  val members: Set[User] = Set() // TODO should include only creator
+  val posts: Set[Post] = Set()
+  // TODO add/remove post
+  // TODO add/remove member
+}
+
+class App() {
+  val loggedInAs: Option[User] = None
+  val room: Option[Room] = None
+  // TODO log in
+  // TODO join room
 }
--- src/test/scala/tutorial/webapp/TutorialTest.scala
+++ src/test/scala/tutorial/webapp/TutorialTest.scala
@@ -2,30 +2,61 @@ package tutorial.webapp
 
 import utest._
 
-import org.scalajs.jquery.jQuery
-
 object TutorialTest extends TestSuite {
 
-  // Initialize App
-  TutorialApp.setupUI()
 
   def tests = TestSuite {
-    'HelloWorld {
-      assert(jQuery("p:contains('Hello World')").length == 1)
+
+    val u = new User("nick")
+
+    'User {
+      assert(u.name == "nick")
+      assert(u.online == false)
+      assert(u.toString() == "nick")
     }
 
-    'ButtonClick {
-      def messageCount =
-        jQuery("p:contains('You clicked the button!')").length
+    'Comment {
+      val c = new Comment(u, "whats good")
+      assert(c.poster== u)
+      assert(c.body == "whats good")
+      assert(c.toString() == "nick - whats good")
+    }
 
-      val button = jQuery("button:contains('Click me!')")
-      assert(button.length == 1)
-      assert(messageCount == 0)
+    'Post {
+      val p = new Post(u, "https://fart.com", Some("Interesting"))
+      assert(p.poster== u)
+      assert(p.url == "https://fart.com")
+      assert(p.title == Some("Interesting"))
+      // TODO check that comments is empty
+      // TODO add a comment, check comments
+      // TODO remove a comment
+      // check that title is optional
+      val p2 = new Post(u, "https://fart.com", None)
+      assert(p2.url == "https://fart.com")
+      assert(p2.title == None)
+    }
 
-      for (c <- 1 to 5) {
-        button.click()
-        assert(messageCount == c)
-      }
+    'Room {
+      val r = new Room(u,  "Great links")
+      assert(r.creator == u)
+      assert(r.name == "Great links")
+      // TODO check that members are just u
+      // TODO add a post
+      // TODO add a member
+      // TODO remove a post
+      // TODO remove a member
+    }
+
+    'App {
+      val a = new App()
+      assert(a.loggedInAs.getOrElse(None) == None)
+      assert(a.room.getOrElse(None) == None)
+      // TODO log in as u
+      // TODO join room r
+      // TODO leave room r
+      // TODO log out as u
+      // TODO log out as u (should fail)
+      // TODO join room when not logged in (should fail)
     }
   }
 }
